78d5ddf8f28c30ecd38ee27cc12e5d97c82fff34
wfsovereign <wfsovereign@outlook.com>
2017-06-18 15:30:50 +0800

    init price view and complete basic function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit, OnChanges {
+
+  cnyValue = 1;
+  usdValue = 50;
+  dataList = [];
+  exchangeRate = 6.8;
+  section = 30;
+  step = 0.2;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.generateDataList(this.cnyValue);
+  }
+
+  generateDataList(cny, usd?) {
+    this.dataList = [];
+    if (cny || cny === 0) {
+      let i = cny;
+      while (i < cny + this.section) {
+        this.dataList.push({
+          cny: i,
+          usd: i / this.exchangeRate
+        });
+        i += this.step;
+      }
+    } else if (usd) {
+      let i = usd;
+      while (i < usd + this.section) {
+        this.dataList.push({
+          cny: i * this.exchangeRate,
+          usd: i
+        });
+        i += this.step;
+      }
+    }
+  }
+
+  cnyConfirm() {
+    this.generateDataList(this.cnyValue);
+  }
+
+  usdConfirm() {
+    this.generateDataList(null, this.usdValue);
+  }
+
+  ngOnChanges() {
+  }
 }
